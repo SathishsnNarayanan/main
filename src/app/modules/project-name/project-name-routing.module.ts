cb952a6249105c2ce0396eb67007ed1d87d72315
vignesh-getstertech <vignesh.k@getster.tech>
2023-09-16 14:37:16 +0530

    fixed

--- src/app/modules/project-name/project-name-routing.module.ts
+++ src/app/modules/project-name/project-name-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectNameComponent } from './project-name/project-name.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'project-name', pathMatch: 'full' },
+  { path: 'project-name', component: ProjectNameComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ProjectNameRoutingModule { }
+export class ProjectNameRoutingModule {}
