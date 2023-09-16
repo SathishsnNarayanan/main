44a6a4a0db4499fa4369d1100c22beae97f8a213
vignesh-getstertech <vignesh.k@getster.tech>
2023-09-16 10:44:24 +0530

    Project Setup Completed.

---
+++ src/app/layout/layout.routing.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('../modules/project-name/project-name.module').then(
+            (m) => m.ProjectNameModule
+          ),
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class LayoutRoutingModule {}
